2268c3a85143fe22e8e95a62a1f5a130f4a803e1
Vikramjeet Singh Dhaliwal <vsdhaliwal2003@gmail.com>
2024-04-19 18:20:45 +0530

    Refactor page component and add sign-in functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,73 +1,30 @@
-'use client';
-import Link from "next/link";
-import style from "../styles/index.module.css";
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { Github } from "lucide-react";
+import { Button } from "~/components/ui/button";
 
-const data = [
-  {name: 'Page A', uv: 4000, pv: 2400, amt: 2400,},
-  {name: 'Page B', uv: 3000, pv: 1398, amt: 2210,},
-  {name: 'Page C', uv: 2000, pv: 9800, amt: 2290,},
-  {name: 'Page D', uv: 2780, pv: 3908, amt: 2000,},
-  {name: 'Page E', uv: 1890, pv: 4800, amt: 2181,},
-  {name: 'Page F', uv: 2390, pv: 3800, amt: 2500,},
-  {name: 'Page G', uv: 3490, pv: 4300, amt: 2100,},
-];
-
-export default function HomePage() {
-  return (
-    <div className={style.parent}>
-      <div className={`${style.div1} border rounded`}>
-      <ResponsiveContainer width="100%" height="100%">
-        <LineChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-        </LineChart>
-      </ResponsiveContainer>
-      </div>
-      <div className={`${style.div2} border rounded`}>
-      <ResponsiveContainer width="100%" height="100%">
-        <LineChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-        </LineChart>
-      </ResponsiveContainer>
-      </div>
-      <div className={`${style.div3} border rounded`}> 
-      <p className={`text-xl font-semibold`}>Recent Forms</p>
-      </div>
-      <div className={`${style.div4} border rounded`}> 
-      <p className={`text-xl font-semibold`}>Pinned</p>
-      </div>
-    </div>
-  );
-}
+export default function Example() {
+    return (
+      <>
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 w-full">
+          <div className="">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            {/* <img
+              className="mx-auto h-10 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              alt="Your Company"
+            /> */}
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              Sign in to your account
+            </h2>
+          </div>
+  
+          <div className="flex justify-center mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <Button variant="outline" className="w-8/12 flex gap-2">
+            <Github></Github>  Sign in with GitHub
+          </Button>
+          </div>
+          </div>
+        </div>
+      </>
+    )
+  }
+  
