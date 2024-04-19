76c7e809feb2bee1ec456b7e9e318617daa88c18
Aman <jaman207168@gmail.com>
2024-04-19 21:55:24 +0530

    edit

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,27 @@
+'use client';
+
 import { type ClassValue, clsx } from "clsx"
+import { useEffect, useState } from "react"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function useMediaQuery(query: string) {
+  const [value, setValue] = useState(false)
+
+  useEffect(() => {
+    function onChange(event: MediaQueryListEvent) {
+      setValue(event.matches)
+    }
+
+    const result = matchMedia(query)
+    result.addEventListener("change", onChange)
+    setValue(result.matches)
+
+    return () => result.removeEventListener("change", onChange)
+  }, [query])
+
+  return value
 }
